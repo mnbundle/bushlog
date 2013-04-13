3c4e35fefc2d16886c0fef2296ac80776c85a4a6
qoda <jpbydendyk@gmail.com>
2013-04-13 02:23:58 +0200

    added gunicorn startup script

--- bushlog/bushlog.sh
+++ bushlog/bushlog.sh
@@ -1,66 +1,12 @@
 #!/bin/sh
 
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-DESC="Guncorn Startup"
-NAME=$(basename $0)
-SCRIPTNAME=/etc/init.d/$NAME
+echo "Restarting the gunicorn instance..."
+kill -9 `cat /tmp/bushlog.pid`
+./bin/gunicorn_django \
+    --workers=3 \
+    --max-requests=250 \
+    --daemon \
+    --log-file=/var/log/gunicorn/bushlog.log \
+    --pid=/tmp/bushlog.pid
+echo "Done."
 
-ROOT=/opt/bushlog
-BIN_PATH=$ROOT/bin
-LOG_PATH=/var/log/gunicorn
-CONTROLSCRIPT=$BIN_PATH/gunicorn_django
-PIDFILE=/tmp/$NAME.pid
-HOST=127.0.0.1
-PORT=8000
-WORKERS=3
-RUN_AS=bushlog
-
-d_start(){
-    if [ -f $PIDFILE ]; then
-        echo -n " already running"
-    else
-        start-stop-daemon --start --quiet \
-            --pidfile $PIDFILE \
-            --chuid $RUN_AS \
-            --exec $CONTROLSCRIPT \
-            --bind=$HOST:$PORT \
-            --workers=$WORKERS \
-            --log-file=$LOG_PATH/bushlog.log \
-            --pid=$PIDFILE
-            --daemon
-        chmod 400 $PIDFILE
-    fi
-}
-
-d_stop(){
-    start-stop-daemon --stop --quiet --pidfile $PIDFILE \
-            || echo -n " not running"
-    if [ -f $PIDFILE ]; then
-        rm $PIDFILE
-    fi
-}
-
-
-case $1 in
-    start)
-    echo -n "Starting $DESC: $NAME"
-    d_start
-    echo "."
-    ;;
-    stop)
-    echo -n "Stopping $DESC: $NAME"
-    d_stop
-    echo "."
-    ;;
-    restart)
-    echo -n "Restarting: $DESC: $NAME"
-    d_stop
-    sleep 1
-    d_start
-    echo "."
-    ;;
-    *)
-    echo "Usage: $0 (start|stop|restart)"
-    exit 1
-    ;;
-esac
