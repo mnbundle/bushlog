5768365cc8bd715d1846d15d1194d0727a2ec542
qoda <jpbydendyk@gmail.com>
2013-04-25 21:37:18 +0200

    bumped version and added more dependancies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
     # project description
     name='bushlog',
-    version='0.9.6',
+    version='0.9.7',
     description='Bushlog Web App',
     long_description="%s\n\n%s" % (open('README.rst', 'r').read(), open('AUTHORS.rst', 'r').read()),
     author='Jonathan Bydendyk',
@@ -26,10 +26,13 @@ setup(
         'PIL==1.1.7',
         'South==0.7.6',
         'argparse==1.2.1',
+        'django-appconf==0.6',
         'django-bootstrap-toolkit==2.8.0',
+        'django-compressor==1.3',
         'django-filter==0.6a1',
         'django-social-auth==0.7.22',
         'djangorestframework==2.2.4',
+        'futures==2.1.3',
         'gunicorn==0.17.2',
         'httplib2==0.8',
         'oauth2==1.5.211',
@@ -40,6 +43,7 @@ setup(
         'simplejson==3.1.3',
         'selenium==2.31.0',
         'six==1.3.0',
+        'versiontools==1.9.1',
         'wsgiref==0.1.2'
     ],
     scripts=[
