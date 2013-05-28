bfa4f6d77cdfd60dbd05710898c682d146d36ab6
qoda <jpbydendyk@gmail.com>
2013-05-28 15:35:45 +0200

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
     # project description
     name='bushlog',
-    version='0.9.8',
+    version='0.9.9',
     description='Bushlog Web App',
     long_description="%s\n\n%s" % (open('README.rst', 'r').read(), open('AUTHORS.rst', 'r').read()),
     author='Jonathan Bydendyk',
