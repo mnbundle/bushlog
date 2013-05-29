dba933cc65cf1b21380dfb8b7e9c486326e36382
qoda <jpbydendyk@gmail.com>
2013-05-29 23:18:38 +0200

    bugfix

--- bushlog/static/js/action.js
+++ bushlog/static/js/action.js
@@ -14,21 +14,23 @@ initLikeButton = function () {
     }
     count_ele.text(current_count);
 
-    btn.click(function () {
-        if (!$(this).hasClass('disabled')) {
-            $.post('/action/like/' + $(this).data('type') + '/', {
-                id: $(this).data('oid'),
-                csrfmiddlewaretoken: csrftoken
-            }, function (data) {
-                if (data.success) {
-                    text_ele.text("Liked");
-                    icon_ele.removeClass('icon-star-empty').addClass('icon-star');
-                    count_ele.text(current_count + 1);
-                    btn.addClass('disabled').unbind('click');
-                }
-            });
-        }
-    });
+    if (btn.data('auth')) {
+        btn.click(function () {
+            if (!$(this).hasClass('disabled')) {
+                $.post('/action/like/' + $(this).data('type') + '/', {
+                    id: $(this).data('oid'),
+                    csrfmiddlewaretoken: csrftoken
+                }, function (data) {
+                    if (data.success) {
+                        text_ele.text("Liked");
+                        icon_ele.removeClass('icon-star-empty').addClass('icon-star');
+                        count_ele.text(current_count + 1);
+                        btn.addClass('disabled').unbind('click');
+                    }
+                });
+            }
+        });
+    }
 }
 
 removePost = function () {
