4f751fbd6a8ebd9f880bff7df97b70af135dd537
qoda <jpbydendyk@gmail.com>
2013-06-09 11:55:08 +0200

    added side panel to combo sighting details

--- bushlog/sitemap.py
+++ bushlog/sitemap.py
@@ -1,4 +1,5 @@
-from django.contrib.sitemaps import GenericSitemap
+from django.contrib import sitemaps
+from django.core.urlresolvers import reverse
 
 from bushlog.apps.profile.models import UserProfile
 from bushlog.apps.reserve.models import Reserve
@@ -6,18 +7,34 @@ from bushlog.apps.sighting.models import Sighting
 from bushlog.apps.wildlife.models import Species
 
 
+class StaticViewSitemap(sitemaps.Sitemap):
+    priority = 0.4
+    changefreq = 'monthly'
+
+    def items(self):
+        return ['index', 'about', 'affiliates', 'legal', 'press', 'support', 'researchers']
+
+    def location(self, item):
+        return reverse(item)
+
+
 sitemaps = {
-    'profiles': GenericSitemap({
-        'queryset': UserProfile.objects.filter(user__is_active=True)
-    }, priority=0.8),
-    'reserves': GenericSitemap({
-        'queryset': Reserve.objects.all()
+    'static': StaticViewSitemap,
+    'profiles': sitemaps.GenericSitemap({
+        'queryset': UserProfile.objects.filter(user__is_active=True),
+        'changefreq': 'weekly'
+    }, priority=0.7),
+    'reserves': sitemaps.GenericSitemap({
+        'queryset': Reserve.objects.all(),
+        'changefreq': 'daily'
     }, priority=0.6),
-    'sightings': GenericSitemap({
+    'species': sitemaps.GenericSitemap({
+        'queryset': Species.objects.all(),
+        'changefreq': 'daily'
+    }, priority=0.6),
+    'sightings': sitemaps.GenericSitemap({
         'queryset': Sighting.objects.active().public(),
-        'date_field': 'date_added'
-    }, priority=0.4),
-    'species': GenericSitemap({
-        'queryset': Species.objects.all()
-    }, priority=0.6),
+        'date_field': 'date_added',
+        'changefreq': 'never'
+    }, priority=0.8),
 }
