f4fbed420f9e2be0ab74e61130f37adbd9dbd79e
qoda <jpbydendyk@gmail.com>
2013-06-10 11:56:41 +0200

    bugfix

--- bushlog/apps/sighting/urls.py
+++ bushlog/apps/sighting/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls import patterns, url
 
 # sighting specific url patterns
 urlpatterns = patterns('bushlog.apps.sighting.views',
-    url(r'^(?P<reserve_slug>\S+)/(?P<species_slug>\S+)/(?P<pk>\d+)/$', 'index', name='index'),
-    url(r'^(?P<reserve_slug>\S+)/(?P<species_slug>\S+)/$', 'combo', name='combo'),
     url(r'^search/$', 'search', name='search'),
     url(r'^create/$', 'create', name='create'),
     url(r'^create/image/$', 'create_image', name='create_image'),
@@ -12,4 +10,6 @@ urlpatterns = patterns('bushlog.apps.sighting.views',
     url(r'^activate/(?P<pk>\d+)/$', 'activate', name='activate'),
     url(r'^deactivate/(?P<pk>\d+)/$', 'deactivate', name='deactivate'),
     url(r'^latest/$', 'latest', name='latest'),
+    url(r'^(?P<reserve_slug>\S+)/(?P<species_slug>\S+)/(?P<pk>\d+)/$', 'index', name='index'),
+    url(r'^(?P<reserve_slug>\S+)/(?P<species_slug>\S+)/$', 'combo', name='combo')
 )
